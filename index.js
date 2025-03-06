ca4ce9c3c3761d8e1a402674cf7896c6ce75af9c
ebenkanin <ebenezerkanin@gmail.com>
2025-03-06 13:21:54 +0900

    group focus fix for input field.

---
+++ index.js
@@ -0,0 +1,17 @@
+/*'Use strict';
+
+const mortgageAmt = document.getElementById('mortgage-amt')
+
+const poundSign = document.querySelector('.pound-sign')
+
+
+mortgageAmt.addEventListener('click',function(){
+    console.log('mortgagem amt input clicked')
+
+    poundSign.style.backgroundColor = '#ced232';
+
+})
+
+mortgageAmt.addEventListener('blur', function () {
+    poundSign.style.backgroundColor = ''; // Reset color when focus is lost
+});*/
